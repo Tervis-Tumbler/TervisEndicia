df59295f46fb74b7f16e51ebca86da6fbd877648
Chris Magnuson <hourglasssand@gmail.com>
2017-03-06 11:56:06 -0500

    Initial commit of Reset-TervisEndiciaSuspendedAccount

---
+++ TervisEndicia.psm1
@@ -0,0 +1,17 @@
+﻿function Reset-TervisEndiciaSuspendedAccount {
+    param (
+        [Parameter(Mandatory)]$NewPassPhrase
+    )
+
+    $RequestID = Get-Random -Minimum 1111111111 -Maximum 9999999999
+    
+    $EndiciaAccount = Get-PasswordstateCredential -PasswordID 3620
+    $EndiciaAccountPassword = $EndiciaAccount.GetNetworkCredential().password
+    
+    $EndiciaAccountChallengeAnswer = Get-PasswordstateCredential -PasswordID 4088
+    $ChallengeAnswer = $EndiciaAccountChallengeAnswer.GetNetworkCredential().password
+
+    $Response = Reset-SuspendedAccountRequestXML -RequesterID lcon -RequestID $RequestID -AccountID $EndiciaAccount.UserName -ChallengeAnswer $ChallengeAnswer -NewPassPhrase $NewPassPhrase
+    $SuspendedAccountRequestXML = [xml]$Response.Content
+    $SuspendedAccountRequestXML.Envelope.Body.ResetSuspendedAccountResponse.ResetSuspendedAccountRequestResponse
+}

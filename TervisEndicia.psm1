2e85f3dab7775871325ab2424ff3f6465957d3fe
Chris Magnuson <hourglasssand@gmail.com>
2017-03-06 16:57:09 -0500

    Add settings.xml file template

--- TervisEndicia.psm1
+++ TervisEndicia.psm1
@@ -19,11 +19,15 @@ function New-Settings1xmlFile {
         [Parameter(Mandatory)]$UserName
     )
     $EndiciaAccount = Get-PasswordstateCredential -PasswordID 3620
+    
     $ElsAccountNumber = $EndiciaAccount.UserName
     $ElsPassPhrase = $EndiciaAccount.GetNetworkCredential().password
     $ElsReturnLabelAccountNumber = $EndiciaAccount.UserName
     $ElsReturnLabelPassPhrase = $EndiciaAccount.GetNetworkCredential().password
     
+    Invoke-ProcessTemplateFile -TemplateFile $PSScriptRoot\Settings.xml.pstemplate |
+    Out-File "\\$ComputerName\C$\Users\$UserName\AppData\Roaming\Endicia\Professional\Profiles\Profile 001\Settings.xml"
+
     Invoke-ProcessTemplateFile -TemplateFile $PSScriptRoot\Settings1.xml.pstemplate |
     Out-File "\\$ComputerName\C$\Users\$UserName\AppData\Roaming\Endicia\Professional\Profiles\Profile 001\Settings1.xml"
 }
